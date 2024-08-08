9f4e3445c553686d1f6e9c161593459a1fa01848
zero <himura.js@hotmail.com>
2024-08-07 22:56:11 -0500

    A save user in Firestore in signUp method

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
-  // instance of auth
+  // instance of auth & firestore
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // sign in
   Future<UserCredential> signInWithEmailPassword(
@@ -20,8 +22,15 @@ class AuthService {
   Future<UserCredential> signUpWithEmailPassword(
       String email, String password) async {
     try {
+      // create user in auth module from Firebase
       UserCredential userCredential = await _auth
           .createUserWithEmailAndPassword(email: email, password: password);
+
+      // save user in Firestore
+      _firestore.collection("Users").doc(userCredential.user!.uid).set({
+        "uuid": userCredential.user!.uid,
+        "email": userCredential.user!.email
+      });
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
