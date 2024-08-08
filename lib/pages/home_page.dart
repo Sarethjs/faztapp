a9dd9d15c6ce8dfad2110dc485a780f9ac00161e
zero <himura.js@hotmail.com>
2024-08-07 22:47:49 -0500

    HomePage shows all users to text them

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,17 @@
+import 'package:faztapp/auth/auth_service.dart';
 import 'package:faztapp/components/my_drawer.dart';
+import 'package:faztapp/components/user_tile.dart';
+import 'package:faztapp/pages/chat_page.dart';
+import 'package:faztapp/services/chat/chat_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  // chat & auth service
+  final ChatService _chatService = ChatService();
+  final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +20,44 @@ class HomePage extends StatelessWidget {
         title: const Text("Home"),
       ),
       drawer: const MyDrawer(),
+      body: _buildUserList(),
     );
+  }
+
+  Widget _buildUserList() {
+    return StreamBuilder(
+        stream: _chatService.getUsersStream(),
+        builder: (context, snapshot) {
+          // error
+          if (snapshot.hasError) {
+            return const Text("Error loading useers");
+          }
+          // loading
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("Loading data");
+          }
+          // return list view
+          return ListView(
+            children: snapshot.data!
+                .map<Widget>(
+                    (userData) => _buildUserListItem(userData, context))
+                .toList(),
+          );
+        });
+  }
+
+  Widget _buildUserListItem(
+      Map<String, dynamic> userData, BuildContext context) {
+    // display all users except current user
+    return UserTile(
+        text: userData["email"],
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ChatPage(
+                        userName: userData["email"],
+                      )));
+        });
   }
 }
