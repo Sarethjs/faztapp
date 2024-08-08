8e81300d1f159e8992ebc0f71342ba3ae62aeb6c
zero <himura.js@hotmail.com>
2024-08-07 22:54:08 -0500

    M returned HomePage as not const

--- lib/auth/auth_gate.dart
+++ lib/auth/auth_gate.dart
@@ -14,7 +14,7 @@ class AuthGate extends StatelessWidget {
         builder: (context, snapshot) {
           //user logged
           if (snapshot.hasData) {
-            return const HomePage();
+            return HomePage();
           }
           // user not logged
           else {
