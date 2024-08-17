9333a353685a2c17592927bfca745a94d03c9a69
zero <himura.js@hotmail.com>
2024-08-17 14:24:03 -0500

    M ToggleTheme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:faztapp/auth/auth_gate.dart';
-import 'package:faztapp/themes/light_mode.dart';
+import 'package:faztapp/themes/theme_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: lightMode,
+      theme: Provider.of<ThemeProvider>(context).themeData,
       home: const AuthGate(),
     );
   }
