f84baba7c2e073f25998476733f1dc696346261a
zero <himura.js@hotmail.com>
2024-08-17 13:24:38 -0500

    A ChatBubble class added

---
+++ lib/components/chat_bubble.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ChatBubble extends StatelessWidget {
+  const ChatBubble({
+    super.key,
+    required this.message,
+    required this.isCurrentUser,
+  });
+
+  final String message;
+  final bool isCurrentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: isCurrentUser ? Colors.green : Colors.grey.shade500,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.symmetric(vertical: 2.5, horizontal: 15),
+      child: Text(
+        message,
+        style: const TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
