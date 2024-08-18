8e0f752bcc1baf65b2ba5041f906eb52caa1e783
zero <himura.js@hotmail.com>
2024-08-18 11:33:21 -0500

    M Options to Scan QR and Add friend added

--- lib/pages/friends_page.dart
+++ lib/pages/friends_page.dart
@@ -9,6 +9,21 @@ class FriendsPage extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
         title: const Text("Friends"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.group_add,
+              size: 28,
+            ),
+          ),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.qr_code_scanner_outlined,
+                size: 28,
+              )),
+        ],
         backgroundColor: Colors.transparent,
         foregroundColor: Theme.of(context).colorScheme.primary,
         elevation: 0,
